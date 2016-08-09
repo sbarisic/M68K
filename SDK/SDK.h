13e51f821ad7fe97d254c7c418468f403f815ab0
Saša Barišić <cartman300@net.hr>
2016-08-09 19:30:13 +0200

    SDK

---
+++ SDK/SDK.h
@@ -0,0 +1,14 @@
+#pragma once
+
+void _start();
+void main();
+
+void _start() {
+	main();
+}
+
+// Define intrinsics
+#define __trap(n) __asm__("trap #" #n ";")
+
+// Custom intrinsics
+#define __bullshit() __asm__(".byte 0b11111111, 0b11111111;")
